500a45fdb3115b8fe773446812e28b0514913d41
KosasihProgramming <growthhuman7@gmail.com>
2024-10-11 16:43:20 +0700

    ddkdkdkdk

--- src/global.js
+++ src/global.js
@@ -1,9 +1,9 @@
 // const protocol = window.location.protocol;
 // const host = "202.157.189.177:5000";
 
-export const urlAPI = `http://localhost:5001`;
+// export const urlAPI = `http://localhost:5001`;
 
-// export const urlAPI = "https://apipresensi-mn76rlbdka-uc.a.run.app";
+export const urlAPI = "https://apipresensi-mn76rlbdka-uc.a.run.app";
 // export const urlAPI = `https://apipresensi.klinikkosasih.com`;
 export const botTokenTelegram =
   "6450512353:AAEPzql6mikTouuEse_S_GhzA7XlmC1lNgE";
